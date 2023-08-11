ddc9bf05f0af7d864073c77749793adb9636face
Adaliz Membreno <adalizitachi11@gmail.com>
2023-08-11 14:21:10 -0600

    init commit

---
+++ components/trendingMovies.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import tw from 'twrnc'
+
+export default function TrendingMovies() {
+  return (
+    <View>
+      <Text style={tw`text-white text-xl mx-4 mb-5`}>Trending</Text>
+    </View>
+  )
+}
---
+++ screens/HomeScreen.js
@@ -0,0 +1,38 @@
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
+import React, {useState} from 'react'
+import tw from 'twrnc'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
+import { Bars3CenterLeftIcon, MagnifyingGlassIcon } from "react-native-heroicons/outline";
+import { styles } from '../theme'
+import TrendingMovies from '../components/trendingMovies'
+
+export default function HomeScreen() {
+    const [trending, setTrendig] = useState([1,2,3])
+    
+    return (
+        <View style={tw`flex-1 bg-neutral-800`}>
+            <SafeAreaView style={tw`mb-3`}>
+                <StatusBar style='light' />
+                <View style={tw`flex-row justify-between items-center mx-4`}>
+                    <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
+                    <Text
+                    style={tw`text-white text-3xl font-bold`}>
+                        <Text style={styles.text}>M</Text>ovies
+                    </Text>
+                    <TouchableOpacity>
+                        <MagnifyingGlassIcon size={30} strokeWidth={2} color="white" />
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+            
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{paddingBottom: 10}}
+            >
+                {/* Movies */}
+                <TrendingMovies data={trending} />
+            </ScrollView>
+        </View>
+    )
+}
